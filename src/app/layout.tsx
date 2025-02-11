35f6550831d922e4b7608b93284cc569e8602a37
Mateo <mateoac439@gmail.com>
2025-02-11 12:11:42 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from './components/Header'; 
+import Header from './components/Header';
 import IntroSection from './components/IntroSection';
 import Projects from './components/Projects';
-import Contact from './components/Contact'; 
+import Contact from './components/Contact';
 import Experience from './components/Experience';
+import About from './components/About';
 // import AboutMe from './components/AboutMe'; 
 
 const geistSans = Geist({
@@ -39,7 +40,7 @@ export default function RootLayout({
       >
         <Header />
         <IntroSection />
-        {/* <AboutMe /> */}
+        <About />
         <Projects />
 
         <Experience />
